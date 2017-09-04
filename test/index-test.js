a4e556f179ab434240a98b0f578b1c9e9eae9c8a
patrick trebat <patrick.trebat@yahoo.com>
2017-09-04 08:27:06 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -17,7 +17,7 @@ describe('what-is-a-test', () => {
   describe('Message', () => {
     it('gives the name and height', () => {
       expect(index.message).toInclude("Susan")
-      expect(index.message).toInclude(80)
+      expect(index.message).toInclude(79)
     })
   })
 
